b777f072086116bd8c67e5ba80e8133ad5bf5881
Abekyo <arbeitmachtfrei7538@gmail.com>
2020-04-11 21:00:48 +0900

    Initial Commit

---
+++ Calculator/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Calculator
+//
+//  Created by 安部晃一朗 on 2020/04/11.
+//  Copyright © 2020 安部晃一朗. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
